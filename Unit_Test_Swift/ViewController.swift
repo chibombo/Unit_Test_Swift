24c09ced61c69ca310a84b91203f2d86eef49208
Luis Genaro Arvizu Vega <genaro.arvizu95@gmail.com>
2018-11-21 12:31:31 -0600

    Initial Commit

---
+++ Unit_Test_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unit_Test_Swift
+//
+//  Created by Luis Genaro Arvizu Vega on 11/21/18.
+//  Copyright © 2018 Profuturo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
