f0bb7590af2d5ee347f88319e3d08c24fc1f344b
Luis Genaro Arvizu Vega <genaro.arvizu95@gmail.com>
2018-11-22 12:08:07 -0600

    Hi8

--- Unit_Test_Swift/ViewController.swift
+++ Unit_Test_Swift/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     
     
     override func viewDidLoad() {
-        print("Hi7")
+        print("Hi8")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
