43a83373e8bdd6ef1f03e7d1958f65b8d51ac712
Luis Genaro Arvizu Vega <genaro.arvizu95@gmail.com>
2018-11-22 18:39:09 -0600

    Hi16

--- Unit_Test_Swift/ViewController.swift
+++ Unit_Test_Swift/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     
     
     override func viewDidLoad() {
-        print("Hi15")
+        print("Hi16")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
